f54d4e0696de19d083fd4db045e4b17d94113559
warathepj <warathepj@gmail.com>
2025-07-31 11:20:03 +0700

    server log form data

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+app.use(express.json()); // Enable parsing of JSON request bodies
 const PORT = process.env.PORT || 3000;
 
 // Serve static files from the 'src' directory
@@ -10,6 +11,14 @@ app.use(express.static(path.join(__dirname, '../src')));
 // Serve index.html for the root URL
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../src', 'index.html'));
+});
+
+app.post('/submit-quiz', (req, res) => {
+    const { keyword, quizLevel } = req.body;
+    console.log('Received quiz submission:');
+    console.log('Keyword:', keyword);
+    console.log('Quiz Level:', quizLevel);
+    res.json({ message: 'Data received successfully!', keyword, quizLevel });
 });
 
 app.listen(PORT, () => {
